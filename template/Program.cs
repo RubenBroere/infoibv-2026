517f6a1c4e9fa16a1e4aa94f5f34a5bff7147c28
Itir Onal Ertugrul <itir@Itirs-MacBook-Air.local>
2026-09-13 13:48:46 +0200

    Add assignment 1 template.

---
+++ template/Program.cs
@@ -0,0 +1,16 @@
+﻿using Avalonia;
+using System;
+
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<ImageApp.App>()
+                     .UsePlatformDetect()
+                     .LogToTrace();
+}
